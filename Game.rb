ec66721d797ef5ebd80a170d6957e5c981fc2ac0
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-29 19:52:18 +0000

    game opening dialogue

--- Game.rb
+++ Game.rb
@@ -1,11 +1,16 @@
 class Game
 
   def initialize
-
+    @player_1 = Player.new(1)
+    @player_2 = Player.new(2)
+    @current_player = @player_1
   end
 
   def start_game
-
+    puts "--------------------------------"
+    puts "Welcome to the Ruby Math Game!!!"
+    puts "--------------------------------"
+    puts "Each player has 3 lives \nYou lose a life when you answer incorrectly\nThe game ends when one person runs out of lives"
   end
 
   def next_turn
