04b40970cacc28bdc02f78e26547141bfc9ee000
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-29 20:49:49 +0000

    fixed typo

--- Question.rb
+++ Question.rb
@@ -1,16 +1,15 @@
 class Question
 
   def initialize
-    @num1 = rand(20) + 1
-    @num2 = rand(20) + 1
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
   end
 
   def question_text
-    "What does #{@numb1} + #{@num2} equal?"
+    "What does #{@num1} + #{@num2} equal?"
   end
 
   def correct_answer
     @num1 + @num2
   end
-  
 end
