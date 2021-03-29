ee494288ddf83b5d851b33254e6ffe62574ebeb7
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-29 20:42:41 +0000

    added a verification

--- Game.rb
+++ Game.rb
@@ -11,22 +11,53 @@ class Game
     puts "Welcome to the Ruby Math Game!!!"
     puts "--------------------------------"
     puts "Each player has 3 lives \nYou lose a life when you answer incorrectly\nThe game ends when one person runs out of lives"
+    ask_to_start
+  end
+
+  def ask_to_start
+    (1..2).each do |id|
+      puts "\nPlease type 'y' to start."
+      puts "\nPlayer #{id}: Ready to start?"
+      print "> "
+      answer = gets.chomp
+      while (answer != "y")
+        puts "Please type 'y' to start. Ready to start?"
+        print "> "
+        answer = gets.chomp
+      end
+    end
+    puts "\n-------- GAME STARTS NOW! --------"
+    next_turn
   end
 
   def next_turn
+    turn = Turn.new(@current_player.id)
+    player_answer = turn.ask_question
 
+    if (player_answer == false)
+      @current_player.lose_life
   end
 
-  def switch_player
+    show_score
 
+    if (@current_player.current_lives == 0)
+      end_game
+    else
+      switch_player
+      puts "\n----- New Turn -----"
+      next_turn
+    end
   end
+  # def switch_player
+
+  # end
 
   def show_score
-
+    puts "P1: #{@player_1.current_lives}/3 vs P2: #{@player_2.current_lives}/3"
   end
 
-  def end_game
+  # def end_game
   
-  end
+  # end
 
 end
