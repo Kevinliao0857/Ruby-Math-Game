7e9080443d22811a3315b5fad1061516cd9c0490
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-29 19:18:10 +0000

    laying out game

---
+++ Game.rb
@@ -0,0 +1,27 @@
+class Game
+
+  def initialize
+
+  end
+
+  def start_game
+
+  end
+
+  def next_turn
+
+  end
+
+  def switch_player
+
+  end
+
+  def show_score
+
+  end
+
+  def end_game
+  
+  end
+
+end
